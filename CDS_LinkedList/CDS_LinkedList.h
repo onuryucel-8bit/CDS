a92e7553d085ccb0f8966d3157bdee8e77ff475d
OnurYucel <39805114+onuryucel-8bit@users.noreply.github.com>
2023-05-19 11:01:10 +0300

    Update CDS_LinkedList.h
    functions definition updates;
    new definition of functions added;

--- CDS_LinkedList/CDS_LinkedList.h
+++ CDS_LinkedList/CDS_LinkedList.h
@@ -17,22 +17,32 @@ struct stdList {
      Node* head;
 };
 
-
+//----------INIT----------//
 List* CDS_LinkedList_init();
 
-//static Node* createNode(int data);
-
+//----------ADD-----------//
 void CDS_LinkedList_addFirst(List* list,void* data);
 void CDS_LinkedList_addLast(List* list,void* data);
 
-void CDS_LinkedList_addAfterIndex(List* list,void* data,int index);
-void CDS_LinkedList_addAfterData();
+void CDS_LinkedList_addAfterIndex(List* list,unsigned int index);
+void CDS_LinkedList_addAfterData(List* list,void* data);
 
+//----------REMOVE-------//
 void CDS_LinkedList_removeFirst(List* list);
 void CDS_LinkedList_removeLast(List* list);
-void CDS_LinkedList_removeAfterIndex();
 
-int CDS_LinkedList_isEmpty();
+void CDS_LinkedList_removeAfterIndex(List* list,unsigned int index);
+void CDS_LinkedList_removeAfterData(List* list,void* data);
+
+//----------UTILS-------//
+int  CDS_LinkedList_isEmpty(List* list);
+void CDS_LinkedList_reverse(List* list);
+void CDS_LinkedList_Compare(List* firstList, List* secondList);
+void* CDS_LinkedList_SearchElement(List* list);
+int  CDS_LinkedList_FindElement(List* list);
+
+///yaklasik olarak [i] islemini yapar
+void* CDS_LinkedList_GetData(List* list,unsigned int index);
 
 void CDS_LinkedList_destroy(List* list);
 
