f23571530a283b64ca74fbe6952d7de683137458
onur yucel <onuryucel200@hotmail.com>
2023-05-19 16:10:59 +0300

    queue functions completed in h

--- CDS_Linear/CDS_Queue/CDS_Queue.c
+++ CDS_Linear/CDS_Queue/CDS_Queue.c
@@ -50,7 +50,30 @@ void CDS_Queue_LL_deQueue(queueLL* queue,void* data){
     if(queue->top == NULL){
         return;
     }
+
+    queueNode* current = queue->top;
+
+    current = current->next;
+
+    free(queue->top);
+
+    queue->top = current;
+
 }
-void CDS_Queue_LL_destroy(queueLL* queue){}
+
+void CDS_Queue_LL_destroy(queueLL* queue){
+
+    queueNode* current = queue->top;
+    queueNode* nextNode = current;
+
+    while(current->next != NULL){
+        nextNode = current->next;
+        free(current);
+        current = nextNode;
+    }
+
+    free(queue);
+    queue->top = NULL;
+}
 
 #endif // CDS_QUEUE_H
