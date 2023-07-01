ec298bf49244cb9604c1afa1d51846c934818999
onur yucel <onuryucel200@hotmail.com>
2023-07-01 20:45:43 +0300

    polynomial.h/c update

--- CDS_LinkedList/CDS_app_polynomial.c
+++ CDS_LinkedList/CDS_app_polynomial.c
@@ -7,6 +7,21 @@ cdst_linkedList_List* CDS_app_polynom_init(){
    return CDS_LinkedList_init();
 }
 
+static int standart_compare_func(void* a,void* b){
+    cds_app_polynom* poly_data = (cds_app_polynom*)(a);
+    cds_app_polynom* cmp_poly_data = (cds_app_polynom*)(b);
+
+    if(poly_data->exponent >= cmp_poly_data->exponent){
+        return 1;
+    }
+
+    return 0;
+}
+
+static cds_app_polynom* to_polynomi(cdst_linkedList_Data* data){
+    return (cds_app_polynom*)(data);
+}
+
 /**
 *  add element to polynom list
 *  it will sort when item added
@@ -22,32 +37,57 @@ void CDS_app_polynom_addElement(cdst_linkedList_List* polynomian,int coefficient
 
     CDS_LinkedList_addLast(polynomian,poly);
 
+    CDS_linkedlist_sort(polynomian,standart_compare_func);
 
-}
-
-static cds_app_polynom* to_polynomi(cdst_linkedList_Data* data){
-    return (cds_app_polynom*)(data);
 }
 
 void CDS_app_polynom_sum(cdst_linkedList_List* poly_sum,cdst_linkedList_List* poly_t){
 
     if(poly_sum == NULL || poly_t == NULL)return;
 
-    cdst_linkedList_Data* current = poly_sum;
-    cdst_linkedList_Data* current_t = poly_t;
+    cdst_linkedList_Data* current = poly_sum->head;
+    cdst_linkedList_Data* current_t = poly_t->head;
+
 
     while(current != NULL || current_t != NULL){
-        if( to_polynomi(current)->exponent ) {
 
-        }
+        //translating void* data to polynom struct
+        cds_app_polynom* poly_current = to_polynomi(current->data);
+        cds_app_polynom* poly_current_t = to_polynomi(current_t->data);
+
+        // ax + bx => a+b
+        poly_current->coefficient = poly_current->coefficient + poly_current_t->coefficient;
+
+        //moving to next nodes
+        current_t = current_t->next;
+        current = current->next;
+    }
+
+}
+
+void CDS_app_polynom_sub(cdst_linkedList_List* poly_sum,cdst_linkedList_List* poly_t){
+
+    if(poly_sum == NULL || poly_t == NULL)return;
+
+    cdst_linkedList_Data* current = poly_sum->head;
+    cdst_linkedList_Data* current_t = poly_t->head;
+
+
+    while(current != NULL || current_t != NULL){
+
+        //translating void* data to polynom struct
+        cds_app_polynom* poly_current = to_polynomi(current->data);
+        cds_app_polynom* poly_current_t = to_polynomi(current_t->data);
+
+        // ax + bx => a+b
+        poly_current->coefficient = poly_current->coefficient - poly_current_t->coefficient;
+
+        //moving to next nodes
+        current_t = current_t->next;
+        current = current->next;
     }
 }
 
-void CDS_app_polynom_sort_forExponent(cdst_linkedList_List* polynomian){
-
-    if(polynomian == NULL)return;
-
-
+void CDS_app_polynom_destroy(cdst_linkedList_List* polyniom){
+    CDS_LinkedList_destroy(polyniom);
 }
-
-void CDS_app_polynom_destroy(){}
--- CDS_LinkedList/CDS_app_polynomial.h
+++ CDS_LinkedList/CDS_app_polynomial.h
@@ -14,12 +14,10 @@ cdst_linkedList_List* CDS_app_polynom_init();
 
 void CDS_app_polynom_addElement(cdst_linkedList_List* polynomian,int coefficient,int exponent);
 
-void CDS_app_polynom_sum();
+void CDS_app_polynom_sum(cdst_linkedList_List* poly_sum,cdst_linkedList_List* poly_t);
 
-void CDS_app_polynom_sort_forExponent();
+void CDS_app_polynom_sub(cdst_linkedList_List* poly_sum,cdst_linkedList_List* poly_t);
 
-void CDS_app_polynom_sort_forCoefficient();
-
-void CDS_app_polynom_destroy();
+void CDS_app_polynom_destroy(cdst_linkedList_List* polyniom);
 
 #endif//CDS_APP_POLYNOMIAL_H
